31d372574e66f86a1217cfb54e7fe6d170249502
Mark Szymczyk <mark@meandmark.com>
2018-11-07 15:07:53 -0500

    Initial Commit

---
+++ RandomNumberGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomNumberGenerator
+//
+//  Created by mark on 11/7/18.
+//  Copyright © 2018 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomNumberGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomNumberGenerator
+//
+//  Created by mark on 11/7/18.
+//  Copyright © 2018 Swift Dev Journal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
